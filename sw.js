91d26121c64845ef73f48c3d12966b0649ddc70a
JY0203 <danny2soug@gmail.com>
2026-06-29 23:47:11 +0800

    fix: UI

--- sw.js
+++ sw.js
@@ -1,30 +1,29 @@
-const CACHE_NAME = 'habit-tracker-v1';
-const ASSETS = [
-  '/habbit-tracker/',
-  '/habbit-tracker/index.html',
-  '/habbit-tracker/manifest.json',
-  '/habbit-tracker/icon-192.png',
-  '/habbit-tracker/icon-512.png'
-];
+const CACHE_NAME = 'habit-tracker-v2';
+const CORE = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())
   );
-  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, copy)).catch(()=>{});
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
